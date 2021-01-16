f89b83a89893209a0bf3cd72a5237854c03f57dc
ssw1ss <Lukas@mac.local>
2021-01-16 18:02:12 -0500

    everything working

--- pages/admin.js
+++ pages/admin.js
@@ -1,28 +1,65 @@
 import { supabase } from '../utils/initSupabase'
 import Layout from "../components/Layout"
+import { useEffect, useState } from 'react'
 
 const Admin = () => {
+  const [name, setName] = useState("")
+  const [startDate, setStartDate] = useState("")
+  const [endDate, setEndDate] = useState("")
+  const [bracket, setBracket] = useState("")
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const { data, error } = await supabase
+      .from("events")
+      .insert([
+        {name, startDate, endDate, bracket}
+      ])
+    if (error) console.log("trevor you fucked up", error)
+    else {
+      console.log("nice!", data)
+    }
+  }
+
   const user = supabase.auth.user()
-  console.log("USER::::", user)
+  useEffect(() => {
+    let timeout
+    if (!user) {
+      timeout = setTimeout(() => {window.location.replace("/")}, 2000)
+    }
+    return () => clearTimeout(timeout)
+  }, [user])
+  if (!user) {
+    return (
+      <Layout>
+        <h1>You must be signed in to view this page</h1>
+      </Layout>
+    )
+  }
   return (
     <Layout>
-      <h1>Admin</h1>
+      <h1>Create Tournament</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" value={name} onChange={e => setName(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="start-date">Start Date</label>
+          <input type="date" id="start-date" value={startDate} onChange={e => setStartDate(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="end-date">End Date</label>
+          <input type="date" id="end-date" value={endDate} onChange={e => setEndDate(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="bracket">Bracket</label>
+          <input type="text" id="bracket" value={bracket} onChange={e => setBracket(e.target.value)} />
+        </div>
+        <button type="submit">Create</button>
+      </form>
     </Layout>
   )
 }
 
 export default Admin
-
-// export async function getServerSideProps({ req }) {
-//   const { user } = await supabase.auth.api.getUserByCookie(req)
-
-//   console.log(user)
-
-//   if (!user) {
-//     // If no user, redirect to index.
-//     return { props: {}, redirect: { destination: '/', permanent: false } }
-//   }
-
-//   // If there is a user, return it.
-//   return { props: { user } }
-// }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
+import { supabase } from '../utils/initSupabase'
 import Layout from "../components/Layout"
 
-const Index = () => {
+const Index = ({ tournaments }) => {
+  console.log(tournaments)
   return (
     <Layout>
-      <h1>Welcome to Lets Melee</h1>
+      <h1>Tournaments</h1>
+      <div className="tournaments">
+        {tournaments.map(({ id, name, startDate, endDate, bracket }) => (
+          <div key={id}>
+            <h3>{name}</h3>
+            <div>From {(new Date(startDate)).toLocaleDateString()} to {new Date(endDate).toLocaleDateString()}</div>
+            <a href={bracket} target="_blank">Bracket Link</a>
+          </div>
+        ))}
+      </div>
     </Layout>
   )
 }
 
 export default Index
+
+export async function getStaticProps() {
+  const { data, error } = await supabase
+    .from("events")
+    .select()
+  if (error) console.log(error)
+  return {
+    props: {
+      tournaments: data
+    }
+  }
+}
